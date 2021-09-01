f4f2c1c7627c624b8a05ae41899d055f3f2f2142
giannisclipper <strunstrun@gmail.com>
2021-09-01 12:45:30 +0300

    Add /src folder to contain the source code.

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const App = () => {
+
+    return (
+        <Text>Under construction...</Text>
+    );
+};
+
+export default App;
