135126756d158aa0aec0b8e7d1b6992dbcccd4e0
giannisclipper <strunstrun@gmail.com>
2021-09-08 14:12:28 +0300

    Replaced http with https, while since Android 10 not allowed simple http requests.

--- src/_commons/constants.js
+++ src/_commons/constants.js
@@ -1,9 +1,9 @@
 const URI = {
-    LINES: 'http://telematics.oasa.gr/api/?act=webGetLines',
-    ROUTES_OF_LINE: 'http://telematics.oasa.gr/api/?act=webGetRoutes&p1=',
-    STOPS_OF_ROUTE: 'http://telematics.oasa.gr/api/?act=webGetStops&p1=',
-    ARRIVALS_OF_STOP: 'http://telematics.oasa.gr/api/?act=getStopArrivals&p1=',
-    ROUTES_OF_STOP: 'http://telematics.oasa.gr/api/?act=webRoutesForStop&p1=',
+    LINES: 'https://telematics.oasa.gr/api/?act=webGetLines',
+    ROUTES_OF_LINE: 'https://telematics.oasa.gr/api/?act=webGetRoutes&p1=',
+    STOPS_OF_ROUTE: 'https://telematics.oasa.gr/api/?act=webGetStops&p1=',
+    ARRIVALS_OF_STOP: 'https://telematics.oasa.gr/api/?act=getStopArrivals&p1=',
+    ROUTES_OF_STOP: 'https://telematics.oasa.gr/api/?act=webRoutesForStop&p1=',
 }
 
 export { URI };
