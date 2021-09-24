8b0c7394f837af06c5f93a40c36a4a5f4a7e315a
giannisclipper <strunstrun@gmail.com>
2021-09-24 12:38:22 +0300

    Rename uniqueList to uniquesArr

--- __tests__/_abstract/uniquesArr.tests.js
+++ __tests__/_abstract/uniquesArr.tests.js
@@ -1,48 +1,48 @@
-const { createUniqueList } = require( '../../src/_abstract/uniqueList' );
+const { createUniquesArr } = require( '../../src/_abstract/uniquesArr' );
 
-describe( 'uniqueList', () => {
+describe( 'uniquesArr', () => {
 
-    let list;
+    let arr;
 
     beforeEach( () => {
-        list = createUniqueList();
-        list.push( 'value 1' );
-        list.push( 'value 2' );
+        arr = createUniquesArr();
+        arr.push( 'value 1' );
+        arr.push( 'value 2' );
     } );
 
     test( 'check a value that exists', async () => {
-        const result = list.includes( 'value 2' );
+        const result = arr.includes( 'value 2' );
         expect( result ).toBe( true );
     } );
 
     test( 'check a value that not exists', async () => {
-        const result = list.includes( 'value 3' );
+        const result = arr.includes( 'value 3' );
         expect( result ).toBe( false );
     } );
 
     test( 'add a value that not exists', async () => {
-        const result = list.push( 'value 3' );
+        const result = arr.push( 'value 3' );
         expect( result ).toBe( true );
     } );
 
     test( 'add a value that exists', async () => {
-        const result = list.push( 'value 2' );
+        const result = arr.push( 'value 2' );
         expect( result ).toBe( false );
     } );
 
     test( 'get all values', async () => {
-        const result = list.getAll();
+        const result = arr.getAll();
         expect( result.length ).toBe( 2 );
     } );
 
     test( 'get one value', async () => {
-        const result = list.getOne( 0 );
+        const result = arr.getOne( 0 );
         expect( result ).toBe( 'value 1' );
     } );
 
-    test( 'clear the list', async () => {
-        list.clear();
-        const result = list.getAll();
+    test( 'clear the arr', async () => {
+        arr.clear();
+        const result = arr.getAll();
         expect( result.length ).toBe( 0 );
     } );
 
--- src/_abstract/uniquesArr.js
+++ src/_abstract/uniquesArr.js
@@ -1,29 +1,29 @@
-const createUniqueList = () => {
+const createUniquesArr = () => {
 
-    const _list = [];
+    const _arr = [];
 
     return {
         includes: function( passval ) {
-            const result = _list.filter( val => val === passval );
+            const result = _arr.filter( val => val === passval );
             return result.length > 0;
         },
 
         push: function( passval ) {
             if ( ! this.includes( passval ) ) {
-               _list.push( passval );
+               _arr.push( passval );
                 return true;
             }
             return false;
         },
 
-        getAll: function() { return _list },
+        getAll: function() { return _arr },
 
-        getOne: function( i ) { return _list[ i ] },
+        getOne: function( i ) { return _arr[ i ] },
 
-        clear: function() { _list.splice( 0, _list.length) },
+        clear: function() { _arr.splice( 0, _arr.length) },
         // details on different ways to clear an array:
         // https://stackoverflow.com/questions/1232040/how-do-i-empty-an-array-in-javascript?rq=1
     }
 }
 
-export { createUniqueList };
+export { createUniquesArr };
